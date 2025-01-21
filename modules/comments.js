ca26fee27834531248fcfa6c32be520f6b16897c
Giorgi-Bahuta <giobahuta@gmail.com>
2025-01-21 19:43:54 +0300

    сделал массив comments пустым, такак работает GET

--- modules/comments.js
+++ modules/comments.js
@@ -1,19 +1,4 @@
-export let comments = [
-    {
-        name: 'Глеб Фокин',
-        date: '12.02.22 12:18',
-        text: 'Это будет первый комментарий на этой странице',
-        isLiked: false,
-        likes: 3,
-    },
-    {
-        name: 'Варвара Н.',
-        date: '13.02.22 19:22',
-        text: 'Мне нравится как оформлена эта страница! ❤',
-        isLiked: true,
-        likes: 75,
-    },
-]
+export let comments = []
 
 export const updateComments = (newComments) => {
     comments = newComments
