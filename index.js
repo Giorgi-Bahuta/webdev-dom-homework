43d2f6bf6ff7c8f3692d8da3f52157bc66d39e26
Giorgi-Bahuta <giobahuta@gmail.com>
2025-02-02 21:22:52 +0300

    Сделал авторизацию

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import { updateComments } from './modules/comments.js'
 import { fetchComments } from './modules/api.js'
 import { addComment } from './modules/post.js'
 
-document.querySelector('.comments').innerHTML =
-    'Список комментариев загружается, пожалуйста подождите'
+// document.querySelector('.comments').innerHTML =
+//     'Список комментариев загружается, пожалуйста подождите'
 
-fetchComments().then((data) => {
-    updateComments(data)
-    renderComments()
-})
-
-addComment()
+export const fetchAndRenderComments = () => {
+    fetchComments().then((data) => {
+        updateComments(data)
+        renderComments()
+    })
+}
+fetchAndRenderComments()
